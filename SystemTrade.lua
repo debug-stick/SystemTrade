032eb8d4fa6d557df97f8ba151949696c242a28f
Lorain.Li <Lorain.Li@icloud.com>
2018-08-23 00:00:00 +0800

    use command for open trade window

--- SystemTrade.lua
+++ SystemTrade.lua
@@ -5,10 +5,7 @@ function Initialize(a_Plugin)
 	a_Plugin:SetName("SystemTrade")
 	a_Plugin:SetVersion(3)
 	g_Plugin = a_Plugin
-	cPluginManager.BindCommand("/SysTrade", "SystemTrade.Trade", OnPlayerOpenTradeWindow, " - open system trade window")
-
-	cPluginManager:AddHook(cPluginManager.HOOK_PLAYER_LEFT_CLICK, MyOnPlayerLeftClick)
-	cPluginManager:AddHook(cPluginManager.HOOK_PLAYER_RIGHT_CLICK, MyOnPlayerRightClick)
+	cPluginManager.BindCommand("/systrade", "SystemTrade.Trade", OnPlayerOpenTradeWindow, " - open system trade window")
 	LOG("SystemTrade v".. g_Plugin:GetVersion() .. " is loaded")
 	return true
 end
@@ -18,28 +15,10 @@ function OnDisable()
 end
 
 function OnPlayerOpenTradeWindow( args,a_Player )
-	local window = cLuaWindows(cWindow.wtChest,9,6,"Trade Windows")
-	window:Setslot(a_Player, 0, cItem(E_ITEM_DIAMOND, 1))
-	--window:SetOnClicked(MyOnWindowClicked)
+	local window = cLuaWindow(cWindow.wtChest,9,6,"Trade Windows")
+	window:SetSlot(a_Player, 0, cItem(E_ITEM_DIAMOND, 1))
+	window:SetOnClicked(MyOnWindowClicked)
 	a_Player:OpenWindow(window)
-end
-
-function MyOnPlayerLeftClick(a_Player, a_BlockX, a_BlockY, a_BlockZ, a_BlockFace, a_Action)
-	local  opendwin = a_Player:GetWindow()
-	if (not (opendwin:GetWindowType() == cWindow.wtChest and opendwin:GetWindowTitle() == "Trade Windows")) then
-		return false
-	end
-	LOG("player clicked " .. a_BlockX .. "," .. a_BlockY .. "," .. a_BlockZ)
-	return true
-end
-
-function MyOnPlayerRightClick(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX, CursorY, CursorZ)
-	local  opendwin = a_Player:GetWindow()
-	if (not (opendwin:GetWindowType() == cWindow.wtChest and opendwin:GetWindowTitle() == "Trade Windows")) then
-		return false
-	end
-	LOG("player clicked " .. a_BlockX .. "," .. a_BlockY .. "," .. a_BlockZ)
-	return true
 end
 
 function MyOnWindowClicked(a_Window, a_Player, a_SlotNum, a_ClickAction, a_ClickedItem)
