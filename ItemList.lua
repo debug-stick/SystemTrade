a3f44850f5588f91e2b01787a92a66f87f275ddc
Lorain.Li <Lorain.Li@icloud.com>
2018-08-23 16:18:18 +0800

    trade item list

---
+++ ItemList.lua
@@ -0,0 +1,21 @@
+--provide trade item list and price
+--{BlockID,Price,unit}
+--players can buy items in the list
+SaleList = 
+{
+	{E_BLOCK_DIAMOND_ORE,1000,1},
+	{E_BLOCK_IRON_ORE,100,1},
+	{E_BLOCK_BEACON,10000,1},
+	{E_BLOCK_COAL_ORE,50,1},
+	{E_BLOCK_SAND,50,1},
+	{E_BLOCK_SNOW,100,1},
+	{E_ITEM_LAVA_BUCKET,10,1},
+}
+--players can sale items in the list
+BuyList=
+{
+	{E_BLOCK_STONE,100,64},
+	{E_BLOCK_CARROTS,200,64},
+	{E_BLOCK_CROPS,200,64},
+	{E_BLOCK_LOG,100,64},
+}
